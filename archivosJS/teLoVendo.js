7e13f24b2bcad389e668dc1ee2b55f530ddf33e4
marcos675r <marcostoromunoz@gmail.com>
2021-09-11 12:20:22 -0300

    Validaciones
    Validación de formulario
    Agregamos Minutos a la hora

--- archivosJS/teLoVendo.js
+++ archivosJS/teLoVendo.js
@@ -1,4 +1,35 @@
-var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour:'numeric',minute:'numeric'};
 var today  = new Date();
 console.log(today.toLocaleDateString("es-ES", options));
 document.getElementById("relojHoy").innerHTML = today.toLocaleDateString("es-ES", options) ;
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("formulario").addEventListener('submit', validarFormulario); 
+  });
+  
+  function validarFormulario(evento) {
+    evento.preventDefault();
+    var nombre = document.getElementById('nombre').value;
+    if(nombre.length == 0) {
+      alert('Ingresa tu nombre');
+      return;
+    }
+    var correo = document.getElementById('telefono').value;
+    if (correo.length == 0) {
+      alert('Ingrese un teléfono válido');
+      return;
+    }
+    var mensaje = document.getElementById('direccion').value;
+    if (mensaje.length == 0) {
+      alert('Ingrese una dirección válida');
+      return;
+    }
+    var mensaje = document.getElementById('correo').value;
+    if (mensaje.length == 0) {
+      alert('Ingrese un correo válido');
+      return;
+    }
+    this.submit();
+  }
