ac214080ed665a7042d5ae0a03ed06c2196bbd8b
marcos675r <marcostoromunoz@gmail.com>
2021-09-14 21:13:55 -0300

    Jquery
    Agregamos Plugins Fancybox
    3 fotos, cada foto tiene 2 tamaños

--- archivosJS/teLoVendo.js
+++ archivosJS/teLoVendo.js
@@ -33,3 +33,34 @@ document.addEventListener("DOMContentLoaded", function() {
     }
     this.submit();
   }
+
+  Fancybox.bind('[data-fancybox="gallery"]', {
+    dragToClose: false,
+  
+    Toolbar: false,
+    closeButton: "top",
+  
+    Image: {
+      zoom: false,
+    },
+  
+    on: {
+      initCarousel: (fancybox) => {
+        const slide = fancybox.Carousel.slides[fancybox.Carousel.page];
+  
+        fancybox.$container.style.setProperty(
+          "--bg-image",
+          `url("${slide.$thumb.src}")`
+        );
+      },
+      "Carousel.change": (fancybox, carousel, to, from) => {
+        const slide = carousel.slides[to];
+  
+        fancybox.$container.style.setProperty(
+          "--bg-image",
+          `url("${slide.$thumb.src}")`
+        );
+      },
+    },
+  });
+ 
